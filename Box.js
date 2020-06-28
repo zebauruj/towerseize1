92dbaa92eecefb85ea09b1da143da409ad77ab8c
zeba <63093390+zebauruj@users.noreply.github.com>
2020-06-28 11:13:00 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,28 @@
+class Box {
+  constructor(x, y) {
+    var options = {
+        'restitution':0.4,
+        'friction':0.0
+        //'density':1.0
+    }
+    this.width = 30;
+    this.height = 40;
+    this.body = Bodies.rectangle(x, y, 30, 40, options);
+    
+    
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    //strokeWeight(4);
+    //stroke("green");
+    fill(random(20,250),random(50,170),random(100,200));
+    rect(0, 0, this.width, this.height);
+    pop();
+  }
+};
